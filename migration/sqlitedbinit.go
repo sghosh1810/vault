0416650573644aa1aa0ae8f57c04d053fb5f7ba4
Shounak Ghosh <sghosh1810@gmail.com>
2025-12-28 14:51:35 +0530

    Update sqlite tables and migration script

--- migration/sqlitedbinit.go
+++ migration/sqlitedbinit.go
@@ -14,91 +14,142 @@ func InitSqliteDB() {
 	defer db.Close()
 
 	initDbScheme := `
-	CREATE TABLE IF NOT EXISTS project ( 
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL UNIQUE,
-		uid TEXT UNIQUE
-		workspace_id INTEGER NOT NULL,
-		FOREIGN KEY (workspace_id) REFERENCES workspace(id)
-			ON DELETE CASCADE ON UPDATE CASCADE
-	);
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			first_name TEXT NOT NULL,
+			last_name TEXT NOT NULL,
+			email TEXT NOT NULL UNIQUE,
+			password_hash TEXT NOT NULL,
+			profile_picture TEXT,
+			created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+		);
+		
+		CREATE TABLE IF NOT EXISTS user_sessions (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			user_id INTEGER NOT NULL,
+			session_id TEXT NOT NULL,
+			refresh_token_hash TEXT NOT NULL,
+			refresh_token_expires_at DATETIME NOT NULL,
+			created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (user_id) REFERENCES users(id)
+				ON DELETE CASCADE ON UPDATE CASCADE
+		);
 
-	CREATE TABLE IF NOT EXISTS secret (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL,
-		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-		workspace_id INTEGER NOT NULL,
-		FOREIGN KEY (workspace_id) REFERENCES workspace(id)
-			ON DELETE CASCADE ON UPDATE CASCADE
-	);
+		CREATE TABLE IF NOT EXISTS workspace (
+			"id"	INTEGER,
+			"name"	TEXT NOT NULL UNIQUE,
+			"description"	TEXT UNIQUE,
+			PRIMARY KEY("id" AUTOINCREMENT)
+		);
 
-	CREATE TABLE IF NOT EXISTS secret_project_map (
-		project_id INTEGER NOT NULL,
-		secret_id INTEGER NOT NULL,
-		PRIMARY KEY (project_id, secret_id),
-		FOREIGN KEY (project_id) REFERENCES project(id)
-			ON DELETE CASCADE ON UPDATE CASCADE,
-		FOREIGN KEY (secret_id) REFERENCES secret(id)
-			ON DELETE CASCADE ON UPDATE CASCADE
-	);
+		CREATE TABLE IF NOT EXISTS user_workspace_access (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			user_id INTEGER NOT NULL,
+			workspace_id INTEGER NOT NULL,
+			has_write_access BOOLEAN NOT NULL,
+			has_share_access BOOLEAN NOT NULL,
+			has_delete_access BOOLEAN NOT NULL,
+			created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (user_id) REFERENCES users(id)
+				ON DELETE CASCADE ON UPDATE CASCADE,
+			FOREIGN KEY (workspace_id) REFERENCES workspace(id)
+				ON DELETE CASCADE ON UPDATE CASCADE
+		);
 
-	CREATE TABLE IF NOT EXISTS secret_version (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		secret_id INTEGER NOT NULL,
-		environment TEXT NOT NULL,
-		version INTEGER NOT NULL,
-		value TEXT NOT NULL,
-		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-		UNIQUE(secret_id, environment, version),
-		FOREIGN KEY(secret_id) REFERENCES secret(id)
-	);
+		CREATE TABLE IF NOT EXISTS project (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL UNIQUE,
+			uid TEXT UNIQUE,
+			workspace_id INTEGER NOT NULL,
+			FOREIGN KEY (workspace_id) REFERENCES workspace(id)
+				ON DELETE CASCADE ON UPDATE CASCADE
+		);
 
-	CREATE TABLE IF NOT EXISTS users (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-        first_name TEXT NOT NULL,
-        last_name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        password_hash TEXT NOT NULL,
-        profile_picture TEXT,
-        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-	);
-	
-	CREATE TABLE IF NOT EXISTS user_sessions (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		user_id INTEGER NOT NULL,
-		session_id TEXT NOT NULL,
-		refresh_token_hash TEXT NOT NULL,
-        refresh_token_expires_at DATETIME NOT NULL,
-		created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		FOREIGN KEY (user_id) REFERENCES users(id)
-			ON DELETE CASCADE ON UPDATE CASCADE
-	);
-	
-	
+		CREATE TABLE IF NOT EXISTS workspace_project_access (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			workspace_id INTEGER NOT NULL,
+			project_id INTEGER NOT NULL,
 
-	CREATE TABLE workspace (
-		"id"	INTEGER,
-		"name"	TEXT NOT NULL UNIQUE,
-		"description"	TEXT UNIQUE,
-		PRIMARY KEY("id" AUTOINCREMENT)
-	);
+			has_read_access BOOLEAN DEFAULT 1,
+			has_write_access BOOLEAN DEFAULT 0,
+			has_share_access BOOLEAN DEFAULT 0,
+			has_delete_access BOOLEAN DEFAULT 0,
 
-	CREATE TABLE IF NOT EXISTS user_workspace_access (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		user_id INTEGER NOT NULL,
-		workspace_id INTEGER NOT NULL,
-		has_write_access BOOLEAN NOT NULL,
-		has_share_access BOOLEAN NOT NULL,
-		has_delete_access BOOLEAN NOT NULL,
-		created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		FOREIGN KEY (user_id) REFERENCES users(id)
-			ON DELETE CASCADE ON UPDATE CASCADE,
-		FOREIGN KEY (workspace_id) REFERENCES workspace(id)
-			ON DELETE CASCADE ON UPDATE CASCADE
-	);
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+			updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+			UNIQUE(workspace_id, project_id),
+
+			FOREIGN KEY (workspace_id)
+				REFERENCES workspace(id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE,
+
+			FOREIGN KEY (project_id)
+				REFERENCES project(id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE
+		);
+
+		CREATE TABLE IF NOT EXISTS secret (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+			workspace_id INTEGER NOT NULL,
+			FOREIGN KEY (workspace_id) REFERENCES workspace(id)
+				ON DELETE CASCADE ON UPDATE CASCADE
+		);
+
+		CREATE TABLE IF NOT EXISTS workspace_secret_access (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			workspace_id INTEGER NOT NULL,
+			secret_id INTEGER NOT NULL,
+
+			has_read_access BOOLEAN DEFAULT 1,
+			has_write_access BOOLEAN DEFAULT 0,
+			has_share_access BOOLEAN DEFAULT 0,
+			has_delete_access BOOLEAN DEFAULT 0,
+
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+			updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+			UNIQUE(workspace_id, secret_id),
+
+			FOREIGN KEY (workspace_id)
+				REFERENCES workspace(id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE,
+
+			FOREIGN KEY (secret_id)
+				REFERENCES secret(id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE
+		);
+
+		CREATE TABLE IF NOT EXISTS secret_project_map (
+			project_id INTEGER NOT NULL,
+			secret_id INTEGER NOT NULL,
+			PRIMARY KEY (project_id, secret_id),
+			FOREIGN KEY (project_id) REFERENCES project(id)
+				ON DELETE CASCADE ON UPDATE CASCADE,
+			FOREIGN KEY (secret_id) REFERENCES secret(id)
+				ON DELETE CASCADE ON UPDATE CASCADE
+		);
+
+		CREATE TABLE IF NOT EXISTS secret_version (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			secret_id INTEGER NOT NULL,
+			environment TEXT NOT NULL,
+			version INTEGER NOT NULL,
+			value TEXT NOT NULL,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+			UNIQUE(secret_id, environment, version),
+			FOREIGN KEY(secret_id) REFERENCES secret(id)
+		);
+
 	`
 
 	_, err = db.Exec(initDbScheme)
